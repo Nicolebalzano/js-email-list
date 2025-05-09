612f227b552b4ec1eafe3994710c1ffb37d97447
Nicolebalzano <nicole.balzano03@gmail.com>
2025-05-09 21:32:26 +0200

    feat: spin and btn added

--- js/script.js
+++ js/script.js
@@ -6,14 +6,34 @@
 // - Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 // - Far sì che le email vengono stampati solo quando arrivano tutti i 10 email
 const emailContainer = document.getElementById("contenitore-email");
+const secondEmailContainer = document.getElementById("secondo-contenitore-email");
+const spinnerElem = document.querySelector(".spinner-border");
+
 
 for (let i = 1; i <= 10; i++) {
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    firstTenEmails = [];
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
 .then((resp) => {   
     const email = resp.data.response;
-    console.log(email);
     
-     emailContainer.innerHTML += `<li>${email}</li>`;
+setTimeout(() => {
+    emailContainer.innerHTML += `<li class="list-group-item list-group-item-action fs-2 mb-0">${email}</li>`;
+   firstTenEmails.push(email);
+   spinnerElem.classList.add("d-none");
+
+}, 2000);      
+
+// buttonElem.addEventListener ("click", moreEmails);
+// function moreEmails(){
+//      axios
+//     .get("https://flynn.boolean.careers/exercises/api/random/mail")
+// .then((resp) => {   
+//     emailContainer.classList.add("d-none");
+//     const secondListEmail = resp.data.response;
+// secondEmailContainer.innerHTML += `<li>${secondListEmail}</li>`})
+
+// }
 })
 
 }
